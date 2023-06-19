43633991400995eac1f6be833e2e2154e96a01d6
Vishaltandale987 <vishaltandle800@gmail.com>
2023-06-19 18:38:29 +0530

    add

--- routes/post.route.js
+++ routes/post.route.js
@@ -135,17 +135,27 @@ postrouter.get("/profile/:username", async (req, res) => {
 
 
 postrouter.put("/:id/comment", async (req, res) => {
+  const postId  = req.params.id;
+  const { username,profilePicture,comment,curenttime } = req.body;
+
   try {
-    const post = await Post.findById(req.params.id);
-    if (!post.likes.includes(req.body.userId)) {
-      await post.updateOne({ $push: { comment: req.body.userId } });
-      res.status(200).json("The post has been liked");
-    } else {
-      await post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(200).json("The post has been disliked");
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
     }
+
+    const newComment = { username, profilePicture, comment,curenttime };
+
+    post.comment.push(newComment);
+
+    await post.save();
+
+    res.status(200).json({ msg: "Comment added successfully" });
+    
   } catch (err) {
-    res.status(500).json(err);
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
 });
 
